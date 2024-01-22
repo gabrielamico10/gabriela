a5873fe38433de624359a14f8f5bc8d88603f1f8
gabrielamico10 <rodicagabriela.mico@edu.upct.es>
2024-01-22 08:34:27 +0100

    Initial page structure

---
+++ gitpractice/RESTcursos/RESTcursos/Controllers/AutoresController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTcursos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        // GET: api/Autores
+        [HttpGet]
+        public IEnumerable<Autor> Get()
+        {
+
+            return new Autor[] {
+                new Autor { DNI = "12345678A", nombre = "Ndelle Labarns", calificacion = 5, thumbnail ="https://multimediarepository.blob.core.windows.net/imagecontainer/fcaa7f1745ad408a90a49c0d081e7aec.jpg"},
+                new Autor { DNI = "12345679B", nombre = "Megan Griffiths ", calificacion = 4, thumbnail ="https://multimediarepository.blob.core.windows.net/imagecontainer/30b3f99246d54ba089be620f46483364.jpg"},
+                new Autor { DNI = "12345671C", nombre = "Erik Adars", calificacion = 4, thumbnail ="https://multimediarepository.blob.core.windows.net/imagecontainer/90c5bd6c9ba348baa9ccb91ad23799b9.jpg"}
+            };
+        }
+
+        
+    }
+}
---
+++ gitpractice/RESTcursos/RESTcursos/Controllers/CategoriasController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTcursos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        // GET: api/Categorias
+        [HttpGet]
+        public IEnumerable<Categoria> Get()
+        {
+            return new Categoria[] {
+                new Categoria { id = 1, nombre = "Programación"},
+                new Categoria { id = 1, nombre = "Matemáticas"},
+                new Categoria { id = 1, nombre = "Biología"}
+            };
+        }
+
+
+
+    }
+}

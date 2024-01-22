2ff2ef81754a125085cadda7e7aa45985dabcbf5
gabrielamico10 <rodicagabriela.mico@edu.upct.es>
2024-01-22 10:10:18 +0100

    m

--- gitpractice/RESTcursos/RESTcursos/obj/Debug/netcoreapp3.1/RESTcursos.AssemblyInfo.cs
+++ gitpractice/RESTcursos/RESTcursos/obj/Debug/netcoreapp3.1/RESTcursos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RESTcursos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5873fe38433de624359a14f8f5bc8d88603f1f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e79d8fd2a7cbb9dcb0f71a2e4e259817bb2f4cfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("RESTcursos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RESTcursos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
